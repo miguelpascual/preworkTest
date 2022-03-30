beebe1c1851b2e69e07702969eda7c31d3459349
Kicoes <anuncios@yahoo.com>
2022-03-30 16:40:51 +0200

    new test

---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
+const projectId = process.env.INFURA_PROJECT_ID;
+const privateKey = process.env.DEPLOYER_SIGNER_PRIVATE_KEY;
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: 'https://rinkeby.infura.io/v3/69414592c6bc4c52bd382350ee0ef9f2',
+      // @kicoes habría que cambiar aquí la url concatenando como abajo pero no me ha funcionado
+      //url: `https://rinkeby.infura.io/v3/${projectId}`,
+      accounts: [privateKey],
+    },  
+  }, 
+};
